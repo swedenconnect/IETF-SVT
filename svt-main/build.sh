444b623c287e9fe8a8b3e96089c54371a84984f3
Stefan Santesson <stefan@aaa-sec.com>
2020-10-22 18:38:47 +0200

    correct build document

--- svt-main/build.sh
+++ svt-main/build.sh
@@ -3,4 +3,4 @@ rm -rf target
 mkdir target
 kramdown-rfc2629 draft-santesson-svt.md >target/draft-santesson-svt.xml
 xml2rfc target/draft-santesson-svt.xml
-cp target/draft-santesson-svt.xml ../draft-santesson-svt.txt
+cp target/draft-santesson-svt.txt ../draft-santesson-svt.txt
